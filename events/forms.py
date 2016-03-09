a9127c8172bbc3f0b7678935f23531b60b6f40da
Vince <libertyhackers@cryptolab.net>
2016-03-09 08:00:15 +0100

    pep8

--- events/forms.py
+++ events/forms.py
@@ -1,15 +1,17 @@
 from django import forms
 from django.core.mail import send_mail
 
+
 class ReservationForm(forms.Form):
     nom = forms.CharField()
     email = forms.EmailField()
     arrivee = forms.DateField(input_formats=['%d/%m/%Y'])
     depart = forms.DateField(input_formats=['%d/%m/%Y'])
     commentaires = forms.CharField(widget=forms.Textarea)
+
     def send_email(self):
         sujet = "Reservation"
-        body ="""Bonjour,\n
+        body = """Bonjour,\n
         Une demande de réservation de l'appartement de St Etiennes a été effectuée depuis le site de l'amicale par {} ({}) pour la période du {} au {}\n
         \n
         ********commentaires********\n
