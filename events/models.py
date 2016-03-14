27f856f479b0f0d055107f476344c93bd9f7cd19
Vince <Vince@blowzy.pw>
2016-03-14 01:55:34 +0100

    ajout du boolean confirmed au modele event

--- events/models.py
+++ events/models.py
@@ -10,6 +10,7 @@ class Event(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField()
     created_by = models.ForeignKey(User)
+    confirmed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
