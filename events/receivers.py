81d45723c987adb23d60bfaf143af7878f32fbf5
Vince <Vince@blowzy.pw>
2016-04-04 02:11:02 +0200

    premier essai pour utiliser les signaux pour bash add events

--- events/receivers.py
+++ events/receivers.py
@@ -7,6 +7,12 @@ from account.signals import user_login_attempt, user_logged_in
 from pinax.eventlog.models import log
 
 
+from django.db.models.signals import pre_save, pre_delete, post_save, post_delete
+from django.contrib.auth.models import User
+from events.models import Event, Reservation
+from datetime import datetime, timedelta
+
+
 @receiver(user_logged_in)
 def handle_user_logged_in(sender, **kwargs):
     log(
@@ -57,3 +63,38 @@ def handle_user_signed_up(sender, **kwargs):
         action="USER_SIGNED_UP",
         extra={}
     )
+
+######################################################
+@receiver(pre_save, sender=Reservation)
+def reservation_pre_save(sender, **kwargs):
+    print('pre_save: {}'.format(kwargs['instance'].__dict__))
+
+@receiver(post_save, sender=Reservation)
+def reservation_post_save(sender, **kwargs):
+    # si user n'est pas connecté
+    user = User.objects.get(username='admin')
+    # sinon si user est connecté
+    # user = request.user
+    if  kwargs['instance'].confirmed == False:
+        nom = '?' + kwargs['instance'].nom + '?'
+    else:
+        nom = kwargs['instance'].nom
+
+    email = kwargs['instance'].email
+    date_debut = kwargs['instance'].date_debut
+    date_fin = kwargs['instance'].date_fin
+
+    jour = datetime.date(date_debut)
+
+    while jour < date_fin or jour == date_fin:
+        Event(title=nom, date=jour, created_by=user).save()
+        jour += timedelta(days=1)
+    print('Saved: {}'.format(kwargs['instance'].__dict__))
+
+@receiver(pre_delete, sender=Reservation)
+def model_pre_delete(sender, **kwargs):
+    print('pre_delete: {}'.format(kwargs['instance'].__dict__))
+
+@receiver(post_delete, sender=Reservation)
+def model_post_delete(sender, **kwargs):
+    print('Deleted: {}'.format(kwargs['instance'].__dict__))
