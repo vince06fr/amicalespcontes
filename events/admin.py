6fee3fabf41e07da963357ffc1e765db7c80ab57
Vince <Vince@blowzy.pw>
2016-04-04 21:00:08 +0200

    customisation de l'administration des Reservations

--- events/admin.py
+++ events/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from events.models import Event, Reservation
 
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'email', 'date_debut', 'date_fin', 'confirmed')
+    date_hierarchy = 'date_debut'
+admin.site.register(Reservation, ReservationAdmin)
 admin.site.register(Event)
-admin.site.register(Reservation)
