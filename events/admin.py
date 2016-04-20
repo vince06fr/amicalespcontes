26913d2617389590ef7bcbd9e3390de2fc6dde0e
Vince <Vince@blowzy.pw>
2016-04-21 00:15:55 +0200

    custom modeladmin pour events

--- events/admin.py
+++ events/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from events.models import Event, Reservation
 
 
+# Reservations
 def set_reservation_validate(modeladmin, request, queryset):
     queryset.update(confirmed=True)
     for event in queryset:
@@ -19,5 +20,22 @@ class ReservationAdmin(admin.ModelAdmin):
     list_display = ('nom', 'email', 'date_debut', 'date_fin', 'confirmed')
     date_hierarchy = 'date_debut'
     actions = [set_reservation_validate, set_reservation_unvalidate]
+
+
+# Events
+def set_event_validate(modeladmin, request, queryset):
+    queryset.update(confirmed=True)
+set_event_validate.short_description = "Valider les events sélectionnés"
+
+def set_event_unvalidate(modeladmin, request, queryset):
+    queryset.update(confirmed=False)
+set_event_unvalidate.short_description = "Annuler la validation des events sélectionnés"
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'confirmed')
+    date_hierarchy = 'date'
+    actions = [set_event_validate, set_event_unvalidate]
+
+
 admin.site.register(Reservation, ReservationAdmin)
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
