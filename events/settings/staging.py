652b69b54a6d8d3baffe1bb9c4cfebaaf612fc46
Vince06fr <vince06fr@gmail.com>
2020-02-02 21:35:22 +0100

    Update configuration for staging and delete production for preventing secrets leak

--- events/settings/staging.py
+++ events/settings/staging.py
@@ -21,10 +21,80 @@ MIDDLEWARE += [
 # Databases
 
 DATABASES = {
-   "default": {
-       "ENGINE": "django.db.backends.sqlite3",
-       "NAME": os.path.join(PROJECT_ROOT, "../database/db.sqlite3"),
-   }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'amicalespcontes',
+        'USER': 'amicalespcontes',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',          # Set to empty string for default.
+    }
+
+}
+
+CACHES = {
+    'default': {
+         'BACKEND':
+'django.core.cache.backends.memcached.PyLibMCCache',
+         'LOCATION': '/tmp/memcached.sock',
+    }
+}
+
+# Todo amelioration
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+ADMINS = ('admin', 'admin@mail.com')
+
+LOGGING = {
+'version': 1,
+'disable_existing_loggers': False,
+'formatters': {
+    'verbose': {
+    'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+    },
+    'simple': {
+    'format': '%(levelname)s %(message)s'
+    },
+},
+'filters': {
+     'require_debug_false': {
+         '()': 'django.utils.log.RequireDebugFalse'
+     }
+ },
+'handlers': {
+    # Include the default Django email handler for errors
+    # This is what you'd get without configuring logging at all.
+    'mail_admins': {
+        'class': 'django.utils.log.AdminEmailHandler',
+        'level': 'ERROR',
+        'filters': ['require_debug_false'],
+         # But the emails are plain text by default - HTML is nicer
+        'include_html': True,
+    },
+    # Log to a text file that can be rotated by logrotate
+    'logfile': {
+        'class': 'logging.handlers.WatchedFileHandler',
+        'filename': '../logs/amicalespcontes.log'
+    },
+},
+'loggers': {
+    # Again, default Django configuration to email unhandled exceptions
+    'django.request': {
+        'handlers': ['mail_admins'],
+                'level': 'ERROR',
+        'propagate': True,
+    },
+    # Might as well log any errors anywhere else in Django
+    'django': {
+        'handlers': ['logfile'],
+        'level': 'ERROR',
+        'propagate': False,
+        },
+    },
+
 }
 
 # Import GMAIL user & password from env variables
