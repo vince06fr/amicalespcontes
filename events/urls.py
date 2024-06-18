9559694316d5b0820b0594fa760cd0db48f9311d
vince06fr <vince06fr@gmail.com>
2024-06-18 16:28:33 +0200

    modify settings & url for serving static in development

--- events/urls.py
+++ events/urls.py
@@ -25,9 +25,11 @@ urlpatterns = [
     url(r"^thanks/", ThankView.as_view(), name="thanks"),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:  # pragma: no cover
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
