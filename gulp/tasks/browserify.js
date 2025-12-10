336754bd9116f8fe7b31467986c08347bd1042b2
vince06fr <vince06fr@gmail.com>
2025-12-10 02:13:26 +0100

    chore: bump jquery and refresh bundles

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -5,6 +5,7 @@ const path = require('path');
 const browserify = require('browserify');
 const babelify   = require('babelify');
 const source     = require('vinyl-source-stream');
+const through2   = require('through2');
 
 /**
  * Module body / Expose
@@ -13,5 +14,12 @@ module.exports = (entry, config) => {
   config = config || {};
   const built = browserify(entry)
     .transform(babelify);
-  return built.bundle().pipe(source(path.basename(entry)));
+  // vinyl-source-stream retourne un objet File sans marqueur _isVinyl attendu par vinyl-fs.
+  // On l'ajoute pour éviter l'erreur "Received a non-Vinyl object in `dest()`".
+  return built.bundle()
+    .pipe(source(path.basename(entry)))
+    .pipe(through2.obj((file, enc, cb) => {
+      file._isVinyl = true;
+      cb(null, file);
+    }));
 };
